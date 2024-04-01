b6e3de3a5e2974085e950dde9c0fcf088bd5c3bd
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2024-04-01 13:17:12 +0530

    changes server.js

--- server.js
+++ server.js
@@ -16,14 +16,11 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 //server static assests in production, for index.html of client to serve
-if (process.env.NODE_ENV === "production") {
-  //Set statuc folder
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 

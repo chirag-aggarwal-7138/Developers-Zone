bbe354a34e9afc10961f752ae520d577f8829fc2
Chirag Aggarwal <aggarwalchirag728@gmail.com>
2024-04-01 13:09:49 +0530

    fixed res.sendFile and created built static folder

---
+++ client/build/precache-manifest.2b6059bf4e435795880f18104c28d93c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51be6670918a0f753ce3c0b7d166559d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157e05a39d4481cb7462",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "eb207357851a0768ce00",
+    "url": "/static/js/2.19162ad0.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.19162ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157e05a39d4481cb7462",
+    "url": "/static/js/main.9cf73d75.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
